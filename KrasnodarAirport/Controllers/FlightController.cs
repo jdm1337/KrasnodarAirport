317cdb8cd1f97e371465c8e4b0fac4cc462a55a3
Sergey <sstaramaly@gmail.com>
2022-07-24 23:57:08 +0300

    Add Flight controller with index view

---
+++ KrasnodarAirport/Controllers/FlightController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KrasnodarAirport.Controllers
+{
+    public class FlightController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
